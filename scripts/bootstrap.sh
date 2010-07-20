3ac330d6418ef299687eb7ba4d7113184505311e
Reid Burke <me@reidburke.com>
2010-07-20 11:29:01 -0700

    Workaround homebrew's broken npm formula.

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -6,7 +6,9 @@ echo "Checking for homebrew..."
 [ ! -e /usr/local/.git ] && ruby -e "$(curl -fsS http://gist.github.com/raw/323731/install_homebrew.rb)"
 
 echo "Installing node and npm."
-brew install node && brew install npm
+# Homebrew and npm don't play nicely at the moment.
+brew install node # && brew install npm
+curl http://npmjs.org/install.sh | sh
 
 echo "You will need to add this directory to your PATH:"
 brew --prefix node
