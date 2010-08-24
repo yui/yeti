7a52569dd7ad2f23e31449428707bfc2cac06816
Reid Burke <me@reidburke.com>
2010-08-23 18:58:16 -0700

    Fix npm path in create-dmg script.

--- scripts/darwin/create-dmg.sh
+++ scripts/darwin/create-dmg.sh
@@ -24,7 +24,7 @@ _NPM_FAKE_ROOT=$PWD/npm
 mkdir -p $_NPM_ROOT/.npm/.cache
 rsync -av ../npm-cache/ $_NPM_ROOT/.npm/
 
-$NODE ~/.node_libraries/.npm/npm/active/package/cli.js \
+$NODE `brew --prefix npm`/libexec/cli.js \
     --root $_NPM_ROOT \
     --binroot $BIN \
     install $YETI
