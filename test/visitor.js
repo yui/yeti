315b8a95b8554374bad829c0443776840a5d3050
Reid Burke <me@reidburke.com>
2010-10-06 23:47:28 -0700

    Add 404 and SyntaxError tests.

--- test/visitor.js
+++ test/visitor.js
@@ -16,8 +16,8 @@ var signal = require("../lib/signal");
 ui.verbose(1); // show debug-level logs
 signal.listen(); // graceful shutdown on SIGINT
 
-function requestTest (fixture) {
-    return {
+function requestTest (fixture, requestOnly) {
+    var suite = {
         topic : request(
             200,
             "/tests/add",
@@ -26,24 +26,29 @@ function requestTest (fixture) {
         ),
         "the test id is returned" : function (id) {
             assert.isString(id);
-        },
-        "and the status is requested" : {
-            topic : request(200, function (id) {
-                return "/status/" + id;
-            }),
-            "the test data is returned" : function (results) {
-                assert.isObject(results);
-                assert.include(results, "passed");
-                assert.include(results, "failed");
-                assert.include(results, "name");
-                assert.include(results, "total");
-            },
-            "the suite passed" : function (result) {
-                assert.ok(result.passed);
-                assert.equal(result.failed, 0);
-            }
         }
     };
+
+    if ( // FIXME: Kludge for tests that just shouldn't stop the runner
+        requestOnly !== true
+    ) suite["and the status is requested"] = {
+        topic : request(200, function (id) {
+            return "/status/" + id;
+        }),
+        "the test data is returned" : function (results) {
+            assert.isObject(results);
+            assert.include(results, "passed");
+            assert.include(results, "failed");
+            assert.include(results, "name");
+            assert.include(results, "total");
+        },
+        "the suite passed" : function (result) {
+            assert.ok(result.passed);
+            assert.equal(result.failed, 0);
+        }
+    };
+
+    return suite;
 }
 
 function requestRunner (transport, browser) {
@@ -65,6 +70,12 @@ function requestRunner (transport, browser) {
         "the server listens to the test add event" : function (listener) {
             assert.isFunction(listener);
         },
+        // Bad tests
+        // TODO: Yeti should report errors in its report for these conditions
+        // Right now, we only verify the test runner skips them okay
+        "and a test with SyntaxError is added" : requestTest("fixture-syntax-error", true),
+        "and a missing test is added" : requestTest("fixture-404", true),
+        // Normal tests
         "and a test is added" : requestTest("fixture"),
         "and a YUI 2.x test is added" : requestTest("fixture-yui2"),
         "and a test with spaces is added" : requestTest("fixture with spaces/fixture again")
