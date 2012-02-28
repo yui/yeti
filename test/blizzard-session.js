18e9b921b5a6274368c3abbc523393809b0ccff7
Reid Burke <me@reidburke.com>
2012-02-28 11:26:51 -0800

    Add incomingBridge test.

--- test/blizzard-session.js
+++ test/blizzard-session.js
@@ -5,6 +5,10 @@ var assert = require("assert");
 
 var blizzard = require("./lib/blizzard");
 
+var BlizzardSession = require("../lib/blizzard/session");
+
+var EventEmitter2 = require("eventemitter2").EventEmitter2;
+
 vows.describe("Blizzard: Socket").addBatch(blizzard.sessionContext({
     "with a simple echo event": {
         topic: blizzard.rpcTopic({
@@ -33,6 +37,32 @@ vows.describe("Blizzard: Socket").addBatch(blizzard.sessionContext({
         },
         "the response should be equal to the fixture": function (topic) {
             assert.deepEqual(topic.res, topic.fixture);
+        }
+    },
+    "with events responded to from the incomingBridge API": {
+        topic: function (lastTopic) {
+            var vow = this,
+                context = {},
+                ee = new EventEmitter2();
+            ee.once("bridge1", function (session, data, reply) {
+                context.session = session;
+                reply(null, data);
+            });
+            lastTopic.server.incomingBridge(ee, "bridge1");
+            lastTopic.client.emit("rpc.bridge1", "quux", function (err, res) {
+                vow.callback(err, {
+                    context: context,
+                    event: this.event,
+                    res: res
+                });
+            });
+        },
+        "the response is correct": function (topic) {
+            assert.strictEqual(topic.event, "rpc.bridge1");
+            assert.strictEqual(topic.res, "quux");
+        },
+        "the BlizzardSession is the first argument to the bridge1 event": function (topic) {
+            assert.ok(topic.context.session instanceof BlizzardSession);
         }
     }
 })).export(module);
