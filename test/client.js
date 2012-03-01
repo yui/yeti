08d61f7de204b4075958f4c2d9d3d4e6bb1af215
Reid Burke <me@reidburke.com>
2012-02-29 16:27:55 -0800

    Add functional test for a batch.

--- test/client.js
+++ test/client.js
@@ -59,7 +59,7 @@ vows.describe("Yeti Client").addBatch({
                 "is ok": function (browser) {
                     assert.isFunction(browser.createPage);
                 },
-                "visits Yeti briefly for the agentConnect event": {
+                "visits Yeti": {
                     topic: function (browser, lastTopic) {
                         var vow = this;
                         browser.createPage(function (page) {
@@ -69,8 +69,10 @@ vows.describe("Yeti Client").addBatch({
 
                             lastTopic.client.once("agentConnect", function (session, agent) {
                                 clearTimeout(timeout);
-                                page.release();
-                                vow.callback(null, agent);
+                                vow.callback(null, {
+                                    page: page,
+                                    agent: agent
+                                });
                             });
 
                             page.open(lastTopic.url, function (status) {
@@ -80,10 +82,61 @@ vows.describe("Yeti Client").addBatch({
                             });
                         });
                     },
-                    "which fires with the agent details": function (agent) {
-                        assert.isString(agent);
+                    "is ok": function (pageTopic) {
+                        assert.ok(pageTopic.page);
+                    },
+                    "which fires agentConnect with the agent details": function (pageTopic) {
+                        assert.isString(pageTopic.agent);
+                    },
+                    "for a batch": {
+                        topic: function (pageTopic, browser, lastTopic) {
+                            var vow = this,
+                                results = [],
+                                agentCompleteFires = 0,
+                                batch = lastTopic.client.createBatch({
+                                    basedir: __dirname + "/fixture",
+                                    tests: ["basic.html"]
+                                });
+
+                            batch.on("agentResult", function (session, agent, details) {
+                                results.push(details);
+                            });
+
+                            batch.on("agentScriptError", function (session, agent, details) {
+                                vow.callback(new Error("Unexpected script error: " + details.message));
+                            });
+
+                            batch.on("agentComplete", function (session, agent) {
+                                agentCompleteFires++;
+                            });
+
+                            batch.on("complete", function () {
+                                pageTopic.page.release();
+                                vow.callback(null, {
+                                    agentResults: results,
+                                    agentCompleteFires: agentCompleteFires
+                                });
+                            });
+                        },
+                        "the agentComplete event fired once": function (topic) {
+                            assert.strictEqual(topic.agentCompleteFires, 1);
+                        },
+                        "the agentResults are well-formed": function (topic) {
+                            assert.isArray(topic.agentResults);
+                            assert.strictEqual(topic.agentResults.length, 1);
+
+                            var result = topic.agentResults[0];
+
+                            assert.include(result, "passed");
+                            assert.include(result, "failed");
+                            assert.include(result, "total");
+                            assert.include(result, "ignored");
+                            assert.include(result, "duration");
+                            assert.include(result, "name");
+                            assert.include(result, "timestamp");
+                        }
                     }
-                },
+                }
                 /* TODO agentDisconnect is not yet implemented.
                 "visits Yeti briefly for the agentDisconnect event": {
                     topic: function (browser, lastTopic) {
