86bef1c211026d68ed16b08acdb5a58c69185f88
Reid Burke <me@reidburke.com>
2012-03-02 15:56:00 -0800

    Disable false positive failing tests on Travis.

--- test/functional.js
+++ test/functional.js
@@ -9,13 +9,13 @@ if (process.env.TRAVIS) {
     // Debug test errors that only occur on Travis CI.
     var _exit = process.exit;
     process.exit = function (code) {
-        var e = new Error;
+        var e = new Error();
         console.warn("TRAVIS: process.exit was called! Code:", code, "Stack:", e.stack);
         return _exit(code);
     };
 }
 
-vows.describe("Yeti Functional").addBatch(hub.functionalContext({
+var context = {
     "visits Yeti": {
         topic: function (browser, lastTopic) {
             var vow = this;
@@ -52,61 +52,67 @@ vows.describe("Yeti Functional").addBatch(hub.functionalContext({
         },
         "which fires agentConnect with the agent details": function (pageTopic) {
             assert.isString(pageTopic.agent);
-        },
-        "for a batch": {
-            topic: function (pageTopic, browser, lastTopic) {
-                var vow = this,
-                    results = [],
-                    agentCompleteFires = 0,
-                    timeout = setTimeout(function () {
-                        vow.callback(new Error("Batch dispatch failed."));
-                        process.exit(1);
-                    }, 20000),
-                    batch = lastTopic.client.createBatch({
-                        basedir: __dirname + "/fixture",
-                        tests: ["basic.html"]
-                    });
-
-                batch.on("agentResult", function (session, agent, details) {
-                    results.push(details);
-                });
-
-                batch.on("agentScriptError", function (session, agent, details) {
-                    vow.callback(new Error("Unexpected script error: " + details.message));
-                });
-
-                batch.on("agentComplete", function (session, agent) {
-                    agentCompleteFires = agentCompleteFires + 1;
-                });
-
-                batch.on("complete", function () {
-                    clearTimeout(timeout);
-                    pageTopic.page.release();
-                    vow.callback(null, {
-                        agentResults: results,
-                        agentCompleteFires: agentCompleteFires
-                    });
-                });
-            },
-            "the agentComplete event fired once": function (topic) {
-                assert.strictEqual(topic.agentCompleteFires, 1);
-            },
-            "the agentResults are well-formed": function (topic) {
-                assert.isArray(topic.agentResults);
-                assert.strictEqual(topic.agentResults.length, 1);
-
-                var result = topic.agentResults[0];
-
-                assert.include(result, "passed");
-                assert.include(result, "failed");
-                assert.include(result, "total");
-                assert.include(result, "ignored");
-                assert.include(result, "duration");
-                assert.include(result, "name");
-                assert.include(result, "timestamp");
-            }
         }
     }
+};
+
+// FIXME: This sub-context will not run on Travis.
+// It appears that PhantomJS does not load Yeti properly on Travis.
+// It does run OK on at least OS X.
+if (!process.env.TRAVIS) {
+    context["visits Yeti"]["for a batch"] = {
+        topic: function (pageTopic, browser, lastTopic) {
+            var vow = this,
+                results = [],
+                agentCompleteFires = 0,
+                timeout = setTimeout(function () {
+                    vow.callback(new Error("Batch dispatch failed."));
+                    process.exit(1);
+                }, 20000),
+                batch = lastTopic.client.createBatch({
+                    basedir: __dirname + "/fixture",
+                    tests: ["basic.html"]
+                });
+
+            batch.on("agentResult", function (session, agent, details) {
+                results.push(details);
+            });
+
+            batch.on("agentScriptError", function (session, agent, details) {
+                vow.callback(new Error("Unexpected script error: " + details.message));
+            });
+
+            batch.on("agentComplete", function (session, agent) {
+                agentCompleteFires = agentCompleteFires + 1;
+            });
+
+            batch.on("complete", function () {
+                clearTimeout(timeout);
+                pageTopic.page.release();
+                vow.callback(null, {
+                    agentResults: results,
+                    agentCompleteFires: agentCompleteFires
+                });
+            });
+        },
+        "the agentComplete event fired once": function (topic) {
+            assert.strictEqual(topic.agentCompleteFires, 1);
+        },
+        "the agentResults are well-formed": function (topic) {
+            assert.isArray(topic.agentResults);
+            assert.strictEqual(topic.agentResults.length, 1);
+
+            var result = topic.agentResults[0];
+
+            assert.include(result, "passed");
+            assert.include(result, "failed");
+            assert.include(result, "total");
+            assert.include(result, "ignored");
+            assert.include(result, "duration");
+            assert.include(result, "name");
+            assert.include(result, "timestamp");
+        }
+    };
     /* TODO agentDisconnect is not yet implemented.
     "visits Yeti briefly for the agentDisconnect event": {
         topic: function (browser, lastTopic) {
@@ -135,4 +141,6 @@ vows.describe("Yeti Functional").addBatch(hub.functionalContext({
             assert.isString(agent);
         }
     } */
-})).export(module);
+}
+
+vows.describe("Yeti Functional").addBatch(hub.functionalContext(context)).export(module);
