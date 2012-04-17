e78bc61931bfca218106143dcd2c9d658735799d
Reid Burke <me@reidburke.com>
2012-04-16 19:14:49 -0700

    Browser JS: Use strict mode, fix JSHint problems.

--- test/fixture/local-js.js
+++ test/fixture/local-js.js
@@ -1,12 +1,17 @@
+/*global YUI */
 YUI().use("test", function (Y) {
-    var LocalJSTestCase = new Y.Test.Case({
+    "use strict";
+    var LocalJSTestCase,
+        LocalJSSuite;
+
+    LocalJSTestCase = new Y.Test.Case({
         name: "Yeti Local JS Test Case",
         testOk: function () {
             Y.Assert.areEqual(1, 1);
         }
     });
 
-    var LocalJSSuite = new Y.Test.Suite("Yeti Local JS Test Suite");
+    LocalJSSuite = new Y.Test.Suite("Yeti Local JS Test Suite");
 
     LocalJSSuite.add(LocalJSTestCase);
 
