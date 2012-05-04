1f900bb57a093f8def28b610d9f860c66656a4e1
Reid Burke <me@reidburke.com>
2012-05-03 18:35:08 -0700

    Add script to run Yeti on YUI. Add go script.

---
+++ go
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+die () {
+    filenames=($(ls -F scripts | grep '*' | sed -e 's/.sh\*//'))
+    scripts=$(printf '\t%s\n' "${filenames[@]}")
+    echo >&2 "$@\n\n\
+usage: $0 script-name [args ...]\n\n\
+Available scripts:\n$scripts"
+    exit 1
+}
+
+[ $# -lt 1 ] && die "No script specified."
+
+script=./scripts/$1.sh
+
+[ -x $script ] || die "Could not execute: $script"
+
+shift 1
+$script $@
---
+++ scripts/yui.sh
@@ -0,0 +1,63 @@
+#!/bin/sh
+
+die () {
+    echo >&2 "$@\nusage: $0 [path-to-yui3] [query]"
+    exit 1
+}
+
+# Node.js and PHP are required.
+hash node 2>&- || die "Could not find Node.js."
+hash php 2>&- || die "Could not find PHP."
+
+CWD=`pwd`
+YETI=$CWD/cli.js
+
+[ -e $YETI ] || die "Yeti not found, expected to find $YETI\n\
+Run this script from Yeti's source directory root."
+
+# Accept two optional arguments: path-to-yui3, search-query.
+if [ "x$1" != "x" ] && [ -d $1 ]; then
+    # First argument is a directory.
+    YUI=$1
+    QUERY=$2
+else
+    # Assume YUI is one level up.
+    YUI="../yui3"
+    QUERY=$1
+fi
+
+[ -d $YUI ] || die "Could not find YUI source at: $YUI\n\
+Provide the directory as the first argument."
+
+[ -e "$YUI/src/common/tests/unit.xml" ] || die "YUI unit.xml not found.\n\
+Make sure this directory contains YUI source: $YUI"
+
+# End of input validation.
+
+cd $YUI
+
+YUI_TESTS="$(php <<'EOPHP'
+<?php
+$txt = file_get_contents("src/common/tests/unit.xml");
+
+$xml = new SimpleXMLElement($txt);
+
+foreach ($xml->tests->url as $item) {
+    print "src/" . (string) $item . "\n";
+}
+?>
+EOPHP)"
+
+if [ "x$QUERY" != "x" ]; then
+    # Only tests matching $QUERY.
+    TESTS=`echo "$YUI_TESTS" | grep $QUERY`
+else
+    # Run everything.
+    TESTS=$YUI_TESTS
+fi
+
+[ ${#TESTS} -gt 0 ] || die "No tests found for query: $QUERY"
+
+echo "Starting Yeti for YUI tests:\n$TESTS"
+
+node $YETI "$TESTS"
