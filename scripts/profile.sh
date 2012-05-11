1828ad4a0d3777c277833ea23b6a0f5f7e2ec1ae
Reid Burke <me@reidburke.com>
2012-05-10 17:12:34 -0700

    Replace profile launcher script with rampant.

--- scripts/profile.sh
+++ scripts/profile.sh
@@ -1,3 +1,19 @@
 #!/bin/sh
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-"$DIR/profile/profile.sh" "$DIR/../cli.js" $@
+
+die () {
+    echo "$@"
+    printf "\nusage: %s [arguments-to-yeti]" $0
+    exit 1
+} >&2
+
+try () {
+    hash npm 2>&- || die "Could not find npm."
+    npm i $1 || die "Unable to install Node.js module $1"
+}
+
+RAMPANT="$DIR/../node_modules/.bin/rampant"
+
+[ -x "$RAMPANT" ] || try rampant
+
+"$RAMPANT" "$DIR/../cli.js" $@
--- scripts/profile/boot.js
+++
@@ -1,5 +0,0 @@
-// Require the profiler and Developer Tools backend.
-require("webkit-devtools-agent");
-
-// Require the requested script specified from STDIN.
-require(require("fs").readFileSync("/dev/stdin").toString().trim());
