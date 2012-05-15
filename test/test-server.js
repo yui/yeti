cab76849430b910497fefe6de9f79084c15f9d11
Reid Burke <me@reidburke.com>
2012-05-15 15:34:58 -0700

    Add test-server injection tests. Ref #64.
    Compares fixtures matching `test/fixture/inject/*.before.html` to its
    `test/fixture/inject/*.after.html` counterpart after running
    TestServer#inject on the `before` markup.
    
    This will be used to test for tricky situations where the first
    script tag on the page is inside a stylesheet or an HTML comment.

---
+++ test/test-server.js
@@ -0,0 +1,105 @@
+"use strict";
+
+var vows = require("vows");
+var assert = require("assert");
+
+var TestServer = require("../lib/hub/test-server");
+
+var PAYLOAD = "<script src=\"foo.js\"></script>";
+
+var fs = require("fs");
+
+function reduceToPairs(files) {
+    var needles = {
+            "before": 1,
+            "after": 2
+        },
+        matches = {};
+
+    // Find pairs of fixtures:
+    //  foo.before.html
+    //  foo.after.html
+    files.forEach(function (file) {
+        var res = /(.*)\.(before|after)\.html$/.exec(file),
+            name,
+            needle;
+
+        if (res === null) {
+            return;
+        }
+
+        name = res[1];
+        needle = res[2];
+
+        if (!(name in matches)) {
+            matches[name] = 0;
+        }
+
+        matches[name] += needles[needle];
+    });
+
+    return Object.keys(matches).filter(function (name) {
+        // Does this match have a before and after?
+        return matches[name] === 3;
+    }).map(function (name) {
+        // Order is important: before, after.
+        return Object.keys(needles).map(function (needle) {
+            return [name, needle, "html"].join(".");
+        });
+    });
+}
+
+function createInjectorTests() {
+    var dir = __dirname + "/fixture/inject",
+        files = fs.readdirSync(dir),
+        pairs = reduceToPairs(files),
+        context = {};
+
+    pairs.forEach(function (pair) {
+        var before = pair[0],
+            after = pair[1];
+        context["when injecting into " + before] = {
+            topic: function (ts) {
+                var vow = this;
+                function onFixtureRead(expected, err, beforeBuffer) {
+                    if (err) {
+                        return vow.callback(err);
+                    }
+
+                    var injectedBuffer = ts.inject(beforeBuffer);
+                    vow.callback(err, {
+                        actual: ts.inject(beforeBuffer).toString("utf8"),
+                        expected: expected
+                    });
+                }
+                fs.readFile([dir, after].join("/"), "utf8", function (err, expected) {
+                    if (err) {
+                        return vow.callback(err);
+                    }
+
+                    // Remove the newline before EOF, if present.
+                    expected = expected.replace(/\n$/, "");
+
+                    fs.readFile([dir, before].join("/"), onFixtureRead.bind(null, expected));
+                });
+            },
+            "the injection is correct": function (topic) {
+                assert.strictEqual(topic.expected, topic.actual);
+            }
+        };
+    });
+
+    return context;
+}
+
+vows.describe("Yeti HTML Injector").addBatch({
+    "A TestServer": {
+        topic: function () {
+            return new TestServer(PAYLOAD);
+        },
+        "is ok": function (ts) {
+            assert.isFunction(ts.inject);
+        },
+        "in use": createInjectorTests()
+    }
+}).export(module);
