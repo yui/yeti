72d7dd774ea25ad565ced28784483f9a075f9262
Clarence Leung <clarle@yahoo-inc.com>
2012-06-04 14:29:23 -0700

    close #72: tests for messenger.js

---
+++ test/messenger.js
@@ -0,0 +1,34 @@
+"use strict";
+
+var vows = require("vows");
+var assert = require("assert");
+var pact = require("pact");
+
+var yeti = require("../lib/yeti");
+
+vows.describe("Yeti Hub HTTP errors").addBatch({
+    "Hub setup and connection" : {
+        topic : pact.httpify(yeti.createHub()),
+        "when a non-GET or HEAD request is used" : {
+            topic : pact.request({
+                url : "/",
+                method : "PUT"
+            }),
+            "should return a 405 error" : pact.code(405),
+            "should return the proper error message" : function (topic) {
+                assert.equal(topic.body, "/* Method Not Allowed\n" +
+                                         "GET or HEAD method required. */");
+            }
+        },
+        "when the desired route is not found" : {
+            topic : pact.request({
+                url : "/foobar"
+            }),
+            "should return a 404 error" : pact.code(404),
+            "should return the proper error message" : function (topic) {
+                assert.equal(topic.body, "/* Not Found\n" + 
+                                         "Unable to find what you're looking for. */");
+            }
+        }
+    }
+}).export(module);
