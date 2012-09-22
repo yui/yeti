a2e4b50c6a394588b005221b93cb52a7970c50d9
Reid Burke <me@reidburke.com>
2012-09-21 18:58:38 -0700

    Use only Istanbul for the code coverage report.
    Istanbul was developed at Yahoo! and is now open source.
    It was used for a while by internal devs if available,
    otherwise the former JSCoverage system would be used.
    
    The JSCoverage fallback is no longer needed.
    
    Istanbul provides many more features than JSCoverage,
    including branch coverage. For more about Istanbul,
    visit: https://github.com/gotwarlost/istanbul

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -2,12 +2,6 @@
 
 istanbul=./node_modules/.bin/istanbul
 
-if [ ! -x $istanbul ]; then
-    if hash istanbul 2>&-; then
-        istanbul=`type -P istanbul`
-    fi
-fi
-
 if [ -x $istanbul ]; then
     echo "Running coverage with Istanbul at $istanbul"
     $istanbul cover \
@@ -15,34 +9,6 @@ if [ -x $istanbul ]; then
         ./node_modules/.bin/vows -- test/*.js
     echo "Coverage report written to build_docs/coverage/index.html"
     hash open 2>&- && open build_docs/coverage/index.html
-    exit 0
+else
+    echo "Istanbul not found. Please run npm install first."
 fi
-
-if [ ! -d tools/jscoverage ]; then
-    mkdir -p tools/jscoverage
-fi
-
-if [ ! -x tools/jscoverage/jscoverage ]; then
-    echo "Installing JSCoverage for Node.js to tools/jscoverage."
-    rm -rf tools/jscoverage/*
-    git clone --quiet https://github.com/visionmedia/node-jscoverage.git tools/jscoverage
-    cd tools/jscoverage
-    ./configure
-    make
-    cd ../..
-fi
-
-rm -rf lib-raw
-cp -R lib lib-raw
-rm -rf lib
-tools/jscoverage/jscoverage lib-raw lib
-./node_modules/.bin/vows --cover-html test/*.js
-rm -rf lib
-mv lib-raw lib
-mkdir -p build_docs
-mv coverage.html build_docs/jscoverage.html
-
-echo "Coverage report written to build_docs/jscoverage.html."
-
-# Open the report on OS X.
-hash open 2>&- && open build_docs/jscoverage.html
