c83fcdff8c34068f1ee768defac1c8dafa2d7c0c
Reid Burke <me@reidburke.com>
2012-10-02 16:53:30 -0700

    lib/cli exports CLI class, expose hooks for UI.
    The CLI class accepts functions and streams in its constructor
    for the implementation to define their own interfaces.
    
    We are doing this so that we can unit test the CLI with
    mock handlers and streams.

--- cli.js
+++ cli.js
@@ -1,2 +1,16 @@
 #!/usr/bin/env node
-require("./lib/cli").route(process.argv);
+
+"use strict";
+
+var CLI = require("./lib/cli").CLI;
+
+var frontend = new CLI({
+    readableStream: process.stdin,
+    writableStream: process.stderr,
+    exitFn: process.exit,
+    errorFn: console.error,
+    putsFn: console.log
+});
+
+frontend.setupExceptionHandler();
+frontend.route(process.argv);
