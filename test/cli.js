283354aa17785cbe3abf6a3d3d53271643af0dca
Reid Burke <me@reidburke.com>
2012-10-03 23:25:55 -0700

    Add basic CLI tests. Ref #95.
    Test the CLI given these conditions:
    
     - Creating a hub.
     - Creating a hub with files.
     - Without arguments.
    
    Caveats: Requires ports 9010 & 9011 to be available.
    
    MockWritableStream was not being used anywhere.
    It has been modified to export the constructor
    instead of a Vows topic function.

--- test/cli.js
+++ test/cli.js
@@ -3,9 +3,95 @@
 var vows = require("vows");
 var assert = require("assert");
 
+var MockWritableStream = require("./lib/writable-stream");
+
 var cli = require("../lib/cli");
+var YetiCLI = cli.CLI;
+
+function cliTopic(fn) {
+    return function () {
+        var topic = {
+            fe: null,
+            writableStream: new MockWritableStream(),
+            log: new MockWritableStream(),
+            error: new MockWritableStream(),
+            exit: 0
+        };
+
+        function mockExit(code) {
+            topic.exit = code;
+        }
+
+
+        topic.fe = new YetiCLI({
+            writableStream: topic.writableStream,
+            readableStream: process.stdin, // FIXME
+            putsFn: topic.log.write.bind(topic.log),
+            errorFn: topic.error.write.bind(topic.error),
+            exitFn: mockExit
+        });
+
+        return fn.call(this, topic);
+    };
+}
 
 vows.describe("Yeti CLI").addBatch({
+    "A Yeti CLI without arguments": {
+        topic: cliTopic(function (topic) {
+            topic.fe.route([
+                "node",
+                "cli.js"
+            ]);
+
+            return topic;
+        }),
+        "returns usage on stderr": function (topic) {
+            assert.ok(topic.error.$store.indexOf("usage:") === 0);
+        },
+        "returns helpful information on stderr": function (topic) {
+            assert.include(topic.error.$store, "launch the Yeti server");
+        }
+    },
+    "A Yeti CLI with --server": {
+        topic: cliTopic(function (topic) {
+            topic.fe.route([
+                "node",
+                "cli.js",
+                "-s",
+                "-p", "9010"
+            ]);
+
+            return topic;
+        }),
+        "returns startup message on stderr": function (topic) {
+            assert.ok(topic.error.$store.indexOf("Yeti Hub started") === 0);
+        }
+    },
+    "A Yeti CLI with files": {
+        topic: cliTopic(function (topic) {
+            var vow = this;
+            topic.fe.route([
+                "node",
+                "cli.js",
+                "-p", "9011",
+                "fixture/basic.html",
+            ]);
+
+            setTimeout(function () {
+                vow.callback(null, topic);
+            }, 250);
+        }),
+        "prints hub creation message on stderr": function (topic) {
+            assert.ok(topic.error.$store.indexOf("Creating a Hub.") === 0);
+        },
+        "waits for agents to connect on stderr": function (topic) {
+            assert.include(topic.error.$store, "Waiting for agents to connect");
+            assert.include(topic.error.$store, "also available locally at");
+        },
+        "prompts on the writableStream": function (topic) {
+            assert.include(topic.writableStream.$store, "When ready, press Enter");
+        }
+    },
     "parseArgv when given arguments": {
         topic: function () {
             return cli.parseArgv([
