564e55fcf76884a138c090ae399bdfbdc8995cab
Reid Burke <me@reidburke.com>
2013-02-06 11:35:39 -0800

    Add CLI#createHub test using Hub mock.
    Use new Mockery dependency to mock the Hub.

--- test/unit/cli.js
+++ test/unit/cli.js
@@ -2,33 +2,94 @@
 
 var vows = require("vows");
 var assert = require("assert");
+var mockery = require("mockery");
+
+var EventEmitter = require("events").EventEmitter;
 
 var streams = require("../lib/streams");
 
-var CLI = require("../../lib/cli").CLI;
+var Configuration = require("../../lib/configuration").Configuration;
 
-var EventEmitter = require("events").EventEmitter;
+function createProcessMock() {
+    var mock = new EventEmitter();
+    mock.cwd = function () {
+        return "/cwd";
+    };
+    return mock;
+}
+
+function createHubMock() {
+    function Hub(options) {
+        this.options = options;
+    }
+
+    return Hub;
+}
 
 vows.describe("Yeti CLI").addBatch({
     "Given a Yeti CLI": {
         topic: function () {
-            return new CLI({
+            var topic = {},
+                cliModulePath = "../../lib/cli",
+                CLI;
+
+            topic.mockProcess = createProcessMock();
+            topic.mockHub = createHubMock();
+
+            mockery.enable();
+            mockery.registerAllowable(cliModulePath);
+            mockery.registerMock('./hub', topic.mockHub);
+
+            CLI = require(cliModulePath).CLI;
+
+            topic.cli = new CLI({
                 stdin: new streams.MockReadableStream(),
                 stdout: new streams.MockWritableStream(),
                 stderr: new streams.MockWritableStream(),
-                process: new EventEmitter()
+                process: topic.mockProcess
             });
+
+            return topic;
+        },
+        teardown: function (topic) {
+            mockery.deregisterAll();
+            mockery.disable();
         },
         "is ok": function (topic) {
             if (topic instanceof Error) { throw topic; }
         },
-        "calling setupExceptionHandler works as expected": function (cli) {
-            var mockProcess = cli.process;
-            assert.lengthOf(mockProcess.listeners("uncaughtException"), 0);
-            cli.setupExceptionHandler();
-            assert.lengthOf(mockProcess.listeners("uncaughtException"), 1);
-            cli.setupExceptionHandler();
-            assert.lengthOf(mockProcess.listeners("uncaughtException"), 1, "Only one handler should be installed.");
+        "calling setupExceptionHandler works as expected": function (topic) {
+            assert.lengthOf(topic.mockProcess.listeners("uncaughtException"), 0);
+            topic.cli.setupExceptionHandler();
+            assert.lengthOf(topic.mockProcess.listeners("uncaughtException"), 1);
+            topic.cli.setupExceptionHandler();
+            assert.lengthOf(topic.mockProcess.listeners("uncaughtException"), 1,
+                "Only one handler should be installed.");
+        },
+        "calling createHub creates a hub": function (topic) {
+            var config,
+                hub,
+                fixture;
+
+            fixture = {
+                loglevel: "foo",
+                "wd-host": "bar",
+                "wd-port": "baz",
+                "wd-user": "quux",
+                "wd-pass": "dogcow"
+            };
+
+            config = new Configuration();
+
+            config.import(fixture);
+
+            hub = topic.cli.createHub(config);
+
+            assert.strictEqual(hub.options.loglevel, fixture.loglevel);
+            assert.strictEqual(hub.options.webdriver.host, fixture["wd-host"]);
+            assert.strictEqual(hub.options.webdriver.port, fixture["wd-port"]);
+            assert.strictEqual(hub.options.webdriver.user, fixture["wd-user"]);
+            assert.strictEqual(hub.options.webdriver.pass, fixture["wd-pass"]);
         }
     }
 }).export(module);
