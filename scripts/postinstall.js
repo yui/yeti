ecce20a65a369cd7be9ed366cd36d75aa17c5766
Reid Burke <me@reidburke.com>
2014-02-07 14:02:58 -0800

    Use GitHub Releases in favor of HISTORY.md.
    Future release notes will be provided at
    https://github.com/yui/yeti/releases

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -4,24 +4,9 @@
 
 var fs = require("fs"),
     path = require("path"),
-    existsSync = fs.existsSync || path.existsSync,
-    history;
-
-function log() {
-    if (process.env.npm_config_loglevel !== "silent") {
-        console.log.apply(null, Array.prototype.slice.call(arguments));
-    }
-}
+    existsSync = fs.existsSync || path.existsSync;
 
 // Avoid fetching deps if possible. See GH-42.
 if (!fs.existsSync(path.join(__dirname, "..", "dep"))) {
     require("./fetch_deps");
 }
-
-fs.readFile(path.join(__dirname, "..", "HISTORY.md"), "utf8", function (err, data) {
-    history = data.split("\n").slice(2, 20).join("\n");
-    process.on("exit", function () {
-        log("\nThanks for installing Yeti", process.env.npm_package_version || 'dev/master');
-        log("\nRecent changes in HISTORY.md:\n\n" + history);
-    });
-});
